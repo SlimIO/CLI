a8e53467782316f40a448f268460b38d51c4e8ea
Malaj Alexandre <alexandre.malaj@gmail.com>
2019-04-10 19:32:03 +0200

    chore: set all require commands in lazy

--- commands/index.js
+++ commands/index.js
@@ -1,11 +1,12 @@
-const initAgent = require("./init");
-const addAddon = require("./addAddon");
-const create = require("./create");
-const connectAgent = require("./connect");
+// Require Third-party Dependencies
+const lazy = require("@slimio/lazy");
 
-module.exports = {
-    initAgent,
-    addAddon,
-    create,
-    connectAgent
-};
+const commands = lazy.of({});
+
+/* eslint-disable global-require */
+commands.set("initAgent", () => require("./init"));
+commands.set("addAddon", () => require("./addAddon"));
+commands.set("create", () => require("./create"));
+commands.set("connectAgent", () => require("./connect"));
+
+module.exports = commands.value;
