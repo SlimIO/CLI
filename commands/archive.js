66437c398378007bcf200bebda15e808b9242257
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-09-23 22:00:59 +0200

    feat: add support for archive command in the root of agent

--- commands/archive.js
+++ commands/archive.js
@@ -7,10 +7,12 @@ const { join } = require("path");
 // Require Third-party Dependencies
 const bundler = require("@slimio/bundler");
 const Manifest = require("@slimio/manifest");
+const qoa = require("qoa");
 const { red, yellow, white, cyan } = require("kleur");
 
 // Require Internal Dependencies
 const { checkBeInAgentOrSubDir, checkBeInAgentDir } = require("../src/utils");
+const { getLocalAddons } = require("../src/agent");
 
 /**
  * @async
@@ -48,17 +50,29 @@ async function archive(addon) {
     }
 
     const { type, name } = Manifest.open();
-    if (type !== "Addon") {
-        console.log(red().bold("Current working dir not detected as an Addon!"));
+    if (type !== "Addon" && name !== "agent") {
+        console.log(red().bold("Current working dir as not been detected as an Addon!"));
         process.exit(0);
     }
 
-    // Verify that we are in SlimIO Agent
-    const rootAgent = join(process.cwd(), "..", "..");
-    process.chdir(rootAgent);
-    checkBeInAgentDir();
+    if (name === "agent") {
+        const localAddons = await getLocalAddons();
+        const { addonName } = await qoa.interactive({
+            query: "For which addon(s) do you want to generate an archive ?",
+            handle: "addonName",
+            menu: [...localAddons]
+        });
+        console.log("");
 
-    await createAddonArchive(process.cwd(), join(rootAgent, "build"), name);
+        await createAddonArchive(process.cwd(), join(process.cwd(), "build"), addonName);
+    }
+    else {
+        const rootAgent = join(process.cwd(), "..", "..");
+        process.chdir(rootAgent);
+        checkBeInAgentDir();
+
+        await createAddonArchive(process.cwd(), join(rootAgent, "build"), name);
+    }
 }
 
 module.exports = archive;
